24806ea7ca1fbe7a621d5f71ff43755775f48f9e
doga akcinar <dogaakcinar@gmail.com>
2023-05-19 00:48:07 +0300

    test2

--- main.go
+++ main.go
@@ -7,12 +7,13 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	// Create two teams
-	team1 := NewTeam("Team 1")
-	team2 := NewTeam("Team 2")
+	team1 := NewTeam("Team 1", true)
+	team2 := NewTeam("Team 2", !team1.IsHomeTeam)
 
 	// The starting team has the kickoff possession
 	team1.HasPossession = true
@@ -23,7 +24,7 @@ func main() {
 		// Print the possession rates
 		team1.PrintPossession(team2)
 
-		// Wait for the operator to change possession
+		// Wait for the operator to change possession or timeout
 		fmt.Print("Press Enter to switch possession or 'q' to quit: ")
 		input, _ := reader.ReadString('\n')
 
@@ -32,12 +33,22 @@ func main() {
 			break
 		}
 
+		// Start the timer
+		startTime := time.Now()
+
 		// Switch the possession to the other team
 		if team1.HasPossession {
 			team2.SwitchPossession(team1)
 		} else {
 			team1.SwitchPossession(team2)
 		}
+
+		// Calculate the elapsed time
+		elapsedTime := time.Since(startTime)
+
+		// Calculate possession rates based on elapsed time
+		team1.PossessionRate = calculatePossessionRate(elapsedTime, team1.IsHomeTeam)
+		team2.PossessionRate = calculatePossessionRate(elapsedTime, team2.IsHomeTeam)
 	}
 
 	fmt.Println("Thank you for using the football possession calculator!")
--- team.go
+++ team.go
@@ -2,35 +2,67 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // Team represents a football team
 type Team struct {
-	Name            string
-	PossessionRate  float64
-	PreviousRate    float64
-	HasPossession   bool
+	Name           string
+	PossessionRate float64
+	PreviousRate   float64
+	HasPossession  bool
+	IsHomeTeam     bool
 }
 
-// NewTeam creates a new team with initial possession rate
-func NewTeam(name string) *Team {
-	return &Team{
-		Name:           name,
-		PossessionRate: 100.0,
-		HasPossession:  false,
+func NewTeam(name string, isHomeTeam bool) *Team {
+	team := &Team{
+		Name:          name,
+		HasPossession: isHomeTeam,
+		IsHomeTeam:    isHomeTeam,
 	}
+
+	if isHomeTeam {
+		team.PossessionRate = 100
+	} else {
+		team.PossessionRate = 0
+	}
+
+	return team
 }
 
 // SwitchPossession switches the possession to the current team
 func (t *Team) SwitchPossession(previousTeam *Team) {
 	t.HasPossession = true
 	t.PreviousRate = previousTeam.PossessionRate
+
 	previousTeam.HasPossession = false
-	previousTeam.PossessionRate /= 2
+	previousTeam.PossessionRate = 100.0 - t.PreviousRate
 }
 
 // PrintPossession prints the possession rates of both teams
 func (t *Team) PrintPossession(otherTeam *Team) {
 	fmt.Printf("Possession: %s - %.2f%% | %s - %.2f%%\n",
 		t.Name, t.PossessionRate, otherTeam.Name, otherTeam.PossessionRate)
+}
+
+func calculatePossessionRate(elapsedTime time.Duration, isHomeTeam bool) float64 {
+	// Calculate the possession rate based on elapsed time
+	const maxPossessionRate = 100.0
+	const totalTime = 10 * time.Second
+
+	if elapsedTime >= totalTime {
+		return maxPossessionRate
+	}
+
+	elapsedSeconds := float64(elapsedTime.Seconds())
+	elapsedRate := elapsedSeconds / totalTime.Seconds()
+	possessionRate := maxPossessionRate * elapsedRate
+
+	if !isHomeTeam {
+		possessionRate = maxPossessionRate - possessionRate
+	}
+
+	return possessionRate
 }
